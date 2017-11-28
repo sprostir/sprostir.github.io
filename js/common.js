dbd3207d27010616c24580ed5e32a47c439ea2b2
sprostir <31355767+sprostir@users.noreply.github.com>
2017-11-29 00:13:50 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $(".header__menu a, .subMenu a, .arrowUp").mPageScroll2id({
+  $(".header__menu a, .arrowUp").mPageScroll2id({
      offset : 65
   });
 
@@ -38,3 +38,77 @@ function initMap() {
            infowindow.open(map, marker);
         });
       }
+
+// Віджет Easypay початок
+  $(function (){
+	  var ServiceId=788;										//ID сервиса
+	  var AccountLabel='Запрошення для введення № рахунку'; 	//Подпись приглашения для ввода номера счета
+    var AccountLengthMin = 1; 								//Минимальная длина приглашения для ввода номера счета
+    var AccountLength = 4; 									//Длина приглашения для ввода номера счета
+	  var AccountDefValue='0678888888';							//Значение по-умолчанию приглашения для ввода номера счета
+	  var AmountLabel='Запрошення для введення суми';			//Подпись приглашения для ввода суммы
+	  var AmountDefValue='5.0';									//Значение по-умолчанию приглашения для ввода суммы
+	  var AmountMin=2;											//Минимальное значение суммы
+	  var AmountMax=5000;										//Максимальное значение суммы
+	  var Locale='ru'; 											//Локаль(ru|ua|en)
+	  var ButtonPayLabel='Сплатити'; 							//Подпись кнопки
+
+	  var Error_Mgs_Account_Is_Empty='Приглашение для ввода номера счета не может быть пустым!';
+
+	  var Error_Mgs_Account_Length_Not_Right='Длина приглашения для ввода номера счета должна быть больше ' + AccountLengthMin +' и меньше ' + AccountLength + '!';
+	  if (AccountLengthMin===AccountLength) {Error_Mgs_Account_Length_Not_Right='Длина приглашения для ввода номера счета должна быть равна ' + AccountLength + '!';}
+	  var Error_Mgs_Amount_Is_Empty='Приглашение для ввода суммы не может быть пустым!';
+	  var Error_Mgs_Amount_Bounds='Сумма должна быть больше ' + AmountMin + ' и меньше ' + AmountMax + '!';
+
+
+	  //-----------------------------------------------------------------------------------------------------------------
+	  $('#service_id').val(ServiceId);
+	  $('#account_label').html(AccountLabel);
+	  $('#account').val(AccountDefValue);
+	  $('#amount_label').html(AmountLabel);
+	  $('#amount').val(AmountDefValue);
+	  $('#lang').val(Locale);
+	  $('#button_pay').html(ButtonPayLabel);
+
+	  $('#account').keyup(function (){
+      var pattern=/[^0-9]/gim;
+	  var newstr=$(this).val().replace(pattern,'');
+	  if (newstr.length > AccountLength) newstr=newstr.slice(0,AccountLength);
+      $(this).val(newstr);
+	  });
+
+	  $('#amount').keyup(function (){
+      var pattern=/^\.|\d+\..*\.|[^\d\.{1}]/;
+      $(this).val($(this).val().replace(pattern,''));
+	  });
+
+	  $('#EasypayWidget').submit(function (){
+	  if ($('#account').val()==''){
+	   $('#error_cell').html(Error_Mgs_Account_Is_Empty);
+	   $('#error_cell').show();
+	   return false;
+	  }
+	  else if ($('#account').val().length < AccountLengthMin || $('#account').val().length > AccountLength){
+	   $('#error_cell').html(Error_Mgs_Account_Length_Not_Right);
+	   $('#error_cell').show();
+	   return false;
+	  }
+      else if ($('#amount').val()=='') {
+	   $('#error_cell').html(Error_Mgs_Amount_Is_Empty);
+	   $('#error_cell').show();
+	   return false;
+	  }
+      else if ($('#amount').val()<=AmountMin) {
+	   $('#error_cell').html(Error_Mgs_Amount_Bounds);
+	   $('#error_cell').show();
+	   return false;
+	  }
+      else if ($('#amount').val()>AmountMax) {
+	   $('#error_cell').html(Error_Mgs_Amount_Bounds);
+	   $('#error_cell').show();
+	   return false;
+	  }
+	  });
+	  });
+
+// Віджет Easypay кінець
